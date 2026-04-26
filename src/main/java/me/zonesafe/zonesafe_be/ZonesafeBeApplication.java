e3e1f0100d37a488db3376fc963764c9dbe1f42a
juhoimda <90125161+juhoimda@users.noreply.github.com>
2026-04-26 22:07:15 +0900

    초기 환경 셋팅

---
+++ src/main/java/me/zonesafe/zonesafe_be/ZonesafeBeApplication.java
@@ -0,0 +1,12 @@
+package me.zonesafe.zonesafe_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonesafeBeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZonesafeBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/zonesafe/zonesafe_be/ZonesafeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.zonesafe.zonesafe_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonesafeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
